aa79c598369176a1c44e95b90217e900a9e9f1d5
nitesis <viviane.bendjus@students.fhnw.ch>
2015-09-28 00:37:46 +0200

    uebung02 pt 1

--- jdraw-assignment/src/jdraw/figures/Rect.java
+++ jdraw-assignment/src/jdraw/figures/Rect.java
@@ -9,9 +9,12 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.util.LinkedList;
 import java.util.List;
 
+
 import jdraw.framework.Figure;
+import jdraw.framework.FigureEvent;
 import jdraw.framework.FigureHandle;
 import jdraw.framework.FigureListener;
 
@@ -21,11 +24,12 @@ import jdraw.framework.FigureListener;
  * @author Christoph Denzler
  *
  */
-public class Rect implements Figure {
+public class Rect implements Figure{
 	/**
 	 * Use the java.awt.Rectangle in order to save/reuse code.
 	 */
 	private java.awt.Rectangle rectangle;
+	private List<FigureListener> listeners = new LinkedList<>();
 	
 	/**
 	 * Create a new rectangle of the given dimension.
@@ -55,12 +59,17 @@ public class Rect implements Figure {
 	public void setBounds(Point origin, Point corner) {
 		rectangle.setFrameFromDiagonal(origin, corner);
 		// TODO notification of change
+		
+		update(new FigureEvent(this));
 	}
 
 	@Override
 	public void move(int dx, int dy) {
+		if(dx != 0 || dy != 0) {
 		rectangle.setLocation(rectangle.x + dx, rectangle.y + dy);
 		// TODO notification of change
+		update(new FigureEvent(this));
+		}		
 	}
 
 	@Override
@@ -84,17 +93,23 @@ public class Rect implements Figure {
 
 	@Override
 	public void addFigureListener(FigureListener listener) {
-		// TODO Auto-generated method stub
+		listeners.add(listener);
 	}
 
 	@Override
 	public void removeFigureListener(FigureListener listener) {
-		// TODO Auto-generated method stub
+		listeners.remove(listener);
 	}
 
 	@Override
 	public Figure clone() {
 		return null;
+	}
+	
+	public void update(FigureEvent e) {
+		for (FigureListener l : listeners) {
+			l.figureChanged(e);
+		}
 	}
 
 }
--- jdraw-assignment/src/jdraw/std/StdDrawModel.java
+++ jdraw-assignment/src/jdraw/std/StdDrawModel.java
@@ -5,59 +5,113 @@
 
 package jdraw.std;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import jdraw.framework.DrawCommandHandler;
 import jdraw.framework.DrawModel;
+import jdraw.framework.DrawModelEvent;
 import jdraw.framework.DrawModelListener;
 import jdraw.framework.Figure;
+import jdraw.framework.FigureEvent;
+import jdraw.framework.FigureListener;
 
 /**
- * Provide a standard behavior for the drawing model. This class initially does not implement the methods
- * in a proper way.
- * It is part of the course assignments to do so.
+ * Provide a standard behavior for the drawing model. This class initially does
+ * not implement the methods in a proper way. It is part of the course
+ * assignments to do so.
+ * 
  * @author TODO add your name here
  *
  */
-public class StdDrawModel implements DrawModel {
+public class StdDrawModel implements DrawModel, FigureListener {
+
+	private List<DrawModelListener> listeners = new LinkedList<>();
+	private List<Figure> figures = new LinkedList<>();
+
+	// // Event handling for green slider
+	// greenSlider.addChangeListener(new ChangeListener() {
+	//
+	// public void stateChanged(ChangeEvent e) {
+	//
+	// int green = greenSlider.getValue();
+	//
+	// setColor(new Color(color.getRed(), green, color.getBlue()));
+	//
+	// }
+	// });
+	//
+	// listeners.add(new ColorListener() {
+	//
+	// @Override
+	// public void colorChanged(Color c) {
+	// greenSlider.setValue(c.getGreen());
+	// }
+	// });
 
 	@Override
 	public void addFigure(Figure f) {
 		// TODO to be implemented
-		System.out.println("StdDrawModel.addFigure has to be implemented");
+		if (f != null && !figures.contains(f)) {
+		f.addFigureListener(this);
+
+		figures.add(f);
+		//hier erfährt view, dass figure hinzugefügt wurde
+		update(new DrawModelEvent(this, f, DrawModelEvent.Type.FIGURE_ADDED));
+		}
+		//System.out.println("StdDrawModel.addFigure has to be implemented");
+		
 	}
 
 	@Override
 	public Iterable<Figure> getFigures() {
-		// TODO to be implemented  
+		// TODO to be implemented
 		System.out.println("StdDrawModel.getFigures has to be implemented");
-		return new LinkedList<Figure>(); // Only guarantees, that the application starts -- has to be replaced !!!
+		return figures; // Only guarantees, that the application starts -- has
+						// to be replaced !!!
 	}
 
 	@Override
 	public void removeFigure(Figure f) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeFigure has to be implemented");
+		// TODO to be implemented
+		// System.out.println("StdDrawModel.removeFigure has to be
+		// implemented");
+		
+		if(figures.remove(f)){
+		update(new DrawModelEvent(this, f, DrawModelEvent.Type.FIGURE_REMOVED));
+		f.removeFigureListener(this);
+		}
 	}
 
 	@Override
 	public void addModelChangeListener(DrawModelListener listener) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.addModelChangeListener has to be implemented");
+		// TODO to be implemented
+		// System.out.println("StdDrawModel.addModelChangeListener has to be
+		// implemented");
+		listeners.add(listener);
 	}
 
 	@Override
 	public void removeModelChangeListener(DrawModelListener listener) {
-		// TODO to be implemented  
-		System.out.println("StdDrawModel.removeModelChangeListener has to be implemented");
+		// TODO to be implemented
+		// System.out.println("StdDrawModel.removeModelChangeListener has to be
+		// implemented");
+		listeners.remove(listener);
 	}
 
-	/** The draw command handler. Initialized here with a dummy implementation. */
+	/**
+	 * The draw command handler. Initialized here with a dummy implementation.
+	 */
 	// TODO initialize with your implementation of the undo/redo-assignment.
 	private DrawCommandHandler handler = new EmptyDrawCommandHandler();
 
 	/**
 	 * Retrieve the draw command handler in use.
+	 * 
 	 * @return the draw command handler.
 	 */
 	public DrawCommandHandler getDrawCommandHandler() {
@@ -66,14 +120,27 @@ public class StdDrawModel implements DrawModel {
 
 	@Override
 	public void setFigureIndex(Figure f, int index) {
-		// TODO to be implemented  
+		// TODO to be implemented
 		System.out.println("StdDrawModel.setFigureIndex has to be implemented");
 	}
 
 	@Override
 	public void removeAllFigures() {
-		// TODO to be implemented  
+		// TODO to be implemented
 		System.out.println("StdDrawModel.removeAllFigures has to be implemented");
+	}
+
+	public void update(DrawModelEvent e) {
+		for (DrawModelListener l : listeners) {
+			l.modelChanged(e);
+		}
+	}
+
+	@Override
+	public void figureChanged(FigureEvent e) {
+		// TODO Auto-generated method stub
+		//???
+		update(new DrawModelEvent(this, e.getFigure(), DrawModelEvent.Type.FIGURE_CHANGED));
 	}
 
 }
