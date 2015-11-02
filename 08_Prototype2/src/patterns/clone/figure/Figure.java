2b01b0b23c4341d0104586a7c09cab9a38d2fcc3
nitesis <viviane.bendjus@students.fhnw.ch>
2015-11-02 12:47:09 +0100

    init Übung cloning Varianten

---
+++ 08_Prototype2/src/patterns/clone/figure/Figure.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2000-2014 Fachhochschule Nordwestschweiz (FHNW)
+ * All Rights Reserved. 
+ */
+
+package patterns.clone.figure;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Base class for figure implementations. Implements listener management.
+ * 
+ */
+public abstract class Figure {
+	private List<FigureListener> listeners = new LinkedList<FigureListener>();
+
+	public void addFigureListener(FigureListener listener) {
+		if (!listeners.contains(listener)) {
+			listeners.add(listener);
+		}
+	}
+
+	public void removeFigureListener(FigureListener listener) {
+		listeners.remove(listener);
+	}
+
+	protected void notifyChange(FigureEvent fe) {
+		for (FigureListener fl : new ArrayList<>(listeners)) {
+			fl.figureChanged(fe);
+		}
+	}
+	
+	public abstract Rectangle getBounds();
+
+}
