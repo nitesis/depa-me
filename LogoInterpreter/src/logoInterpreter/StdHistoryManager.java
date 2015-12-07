3933a52ebc9d3c5d8d5353fbc661fd1cd1b396fc
nitesis <viviane.bendjus@students.fhnw.ch>
2015-12-07 16:27:52 +0100

    first part implementation
    still buggy

--- LogoInterpreter/src/logoInterpreter/StdHistoryManager.java
+++ LogoInterpreter/src/logoInterpreter/StdHistoryManager.java
@@ -1,21 +1,28 @@
 package logoInterpreter;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import logoInterpreter.commands.Command;
 
 public class StdHistoryManager implements HistoryManager {
 	private final LogoInterpreter interpreter;
+	
+	public LinkedList<Command> commandList = new LinkedList<Command>();
+	public int indexCommand = 0;
 	
 	public StdHistoryManager(LogoInterpreter interpreter) {
 		this.interpreter = interpreter;
 	}
 	/**
 	 * Fügt einen Command in die History ein. Dieser Command wird bereits im 
-	 * LogoInterpreter ausgeführt, darf also hier nich nochmals ausgeführt werden.
+	 * LogoInterpreter ausgeführt, darf also hier nicht nochmals ausgeführt werden.
 	 */
 	@Override
 	public void add(Command command) {
 		// TODO Auto-generated method stub
-
+		commandList.add(command);
+		indexCommand++;
 	}
 
 	/**
@@ -24,6 +31,11 @@ public class StdHistoryManager implements HistoryManager {
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
+		Iterator<Command> it = commandList.iterator();
+		while(it.hasNext()) {
+			it.remove();
+			it.next();
+		}
 
 	}
 	/**
@@ -33,7 +45,14 @@ public class StdHistoryManager implements HistoryManager {
 	@Override
 	public void undo() {
 		// TODO Auto-generated method stub
-
+		if(commandList.getLast() != null) {
+			Command undo = commandList.getLast();
+			undo.execute();
+			indexCommand--;
+			
+			}else{
+				System.out.println("Undo nicht möglich.");
+			}
 	}
 
 	/**
@@ -43,6 +62,12 @@ public class StdHistoryManager implements HistoryManager {
 	@Override
 	public void redo() {
 		// TODO Auto-generated method stub
+		Command redo = commandList.get(indexCommand++);
+		if(redo != null) {
+			redo.execute();
+		}else{
+			System.out.println("Redo nicht möglich.");
+		}
 
 	}
 
@@ -53,7 +78,7 @@ public class StdHistoryManager implements HistoryManager {
 	@Override
 	public Iterable<Command> getCommands() {
 		// TODO Auto-generated method stub
-		return null;
+		return commandList;
 	}
 
 }
